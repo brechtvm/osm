95dfbecae92716d621e2764aabcdd6e3162b2424
Hanno Hecker <vetinari@ankh-morp.org>
2015-07-17 19:06:58 +0200

    add Contains(*Point)

--- bbox/bbox.go
+++ bbox/bbox.go
@@ -15,4 +15,9 @@ func (b *BBox) String() string {
 		b.LowerLeft.Lat, b.LowerLeft.Lon, b.UpperRight.Lon, b.UpperRight.Lat, "")
 }
 
+func (b *BBox) Contains(p *Point) bool {
+	return p.Lat > b.LowerLeft.Lat && p.Lat < b.UpperRight.Lat &&
+		p.Lon > b.LowerLeft.Lon && p.Lon < b.UpperRight.Lon
+}
+
 // vim: ts=4 sw=4 noexpandtab nolist syn=go
