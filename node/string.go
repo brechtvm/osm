20d62add4d5fa6c28f977e4f69c1f83c3d062b95
Brecht Van Maldergem <Brecht.VanMaldergem@be-mobile.com>
2017-03-10 16:31:47 +0100

    fix: html.EscapeString

--- node/string.go
+++ node/string.go
@@ -2,13 +2,14 @@ package node
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
 	"time"
 )
 
 // OSM XML output of a node
 func (n *Node) String() string {
 	s := fmt.Sprintf(`  <node id="%d" timestamp="%s" uid="%d" user="%s" visible="%t" version="%d" changeset="%d" lat="%f" lon="%f"`,
-		n.Id_, n.Timestamp_.Format(time.RFC3339), n.User_.Id, n.User_.Name, n.Visible_,
+		n.Id_, n.Timestamp_.Format(time.RFC3339), n.User_.Id, html.EscapeString(n.User_.Name), n.Visible_,
 		n.Version_, n.Changeset_, n.Position_.Lat, n.Position_.Lon)
 	t := n.Tags_.String()
 	if t == "" {
