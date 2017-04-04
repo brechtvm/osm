ae80e8f03ab7987eb4c3ab6fed5d89dda39ccb36
Brecht Van Maldergem <Brecht.VanMaldergem@be-mobile.com>
2017-04-04 09:03:47 +0200

    fix: limit to 4 CPU Cores

--- pbf/parse.go
+++ pbf/parse.go
@@ -52,7 +52,8 @@ func (p *Pbf) Parse() (o *osm.OSM, err error) {
 
 	d := osmpbf.NewDecoder(p.r)
 
-	err = d.Start(runtime.GOMAXPROCS(-1))
+	//err = d.Start(runtime.GOMAXPROCS(-1))
+	err = d.Start(runtime.GOMAXPROCS(4))
 	if err != nil {
 		return
 	}
